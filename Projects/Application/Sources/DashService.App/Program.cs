1a996cb3a0d8a7e64c9aaac186082032f0294fd7
Saeed Mohammadi <s.mohammadi@mabnadp.com>
2021-02-03 15:58:56 +0330

    Inject jobs at runtime

--- Projects/Application/Sources/DashService.App/Program.cs
+++ Projects/Application/Sources/DashService.App/Program.cs
@@ -1,6 +1,13 @@
-﻿using Autofac;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -32,6 +39,31 @@ namespace DashService.App
         public static void ConfigureContainer(ContainerBuilder builder)
         {
             Logger.DependencyRegistration.RegisterModules(builder);
+            RegisterJobs(builder);
+        }
+
+        private static void RegisterJobs(ContainerBuilder builder)
+        {
+            string[] assemblyScannerPattern = new[] { @"DashService.Job.*.dll" };
+
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+            List<Assembly> assemblies = new List<Assembly>();
+            assemblies.AddRange(
+                Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*.dll", SearchOption.AllDirectories)
+                    .Where(filename => assemblyScannerPattern.Any(pattern => 
+                        Regex.IsMatch(filename, pattern)
+                        && !Regex.IsMatch(filename, "DashService.Job.dll")
+                        && !Regex.IsMatch(filename, "DashService.Job.Abstraction.dll")
+                    ))
+                    .Select(Assembly.LoadFrom)
+            );
+
+            foreach (var assembly in assemblies)
+            {
+                builder.RegisterAssemblyTypes(assembly)
+                    .AsImplementedInterfaces();
+            }
         }
     }
 }
